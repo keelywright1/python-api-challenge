4580e9552cee7df30fd988c1220c0498f59b1021
keelywright <keelywright1@gmail.com>
2021-04-22 20:57:37 -0700

    api keys and weather py organization

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a794a944850b964d7faba730eda6c9f5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCC_8OC7V0Ro4BdGioP0ZEBHwkwiYWFO78"
